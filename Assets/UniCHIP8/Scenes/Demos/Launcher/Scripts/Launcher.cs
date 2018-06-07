ddef5384a9626fb9ca698922702c3872e762fffa
Scott Elcomb <psema4@gmail.com>
2018-06-07 19:00:03 -0400

    add rocket launch controller demo

---
+++ Assets/UniCHIP8/Scenes/Demos/Launcher/Scripts/Launcher.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Launcher : MonoBehaviour {
+	public GameObject rocket;
+	private bool launched = false;
+	Rigidbody rb;
+	Rocket r;
+
+	void Start() {
+		r = rocket.GetComponent<Rocket> ();
+		rb = rocket.GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate() {
+		if (launched && r.fuel > 0) {
+			rb.AddForce(Vector3.up * (r.thrust * Time.deltaTime), ForceMode.Acceleration);
+		}
+	}
+
+	public void LaunchRocket() {
+		print ("Launching rocket...");
+		r.Launch ();
+		launched = true;
+	}
+
+}
---
+++ Assets/UniCHIP8/Scenes/Demos/Launcher/Scripts/Rocket.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rocket : MonoBehaviour {
+	public int fuel = 500;
+	private int startingFuel;
+	[HideInInspector]
+	public float thrust;
+	private bool inFlight = false;
+	private Rigidbody rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+		startingFuel = fuel;
+	}
+
+	void FixedUpdate () {
+		if (inFlight) {
+			thrust = fuel / 10f;
+			rb.AddForce (Vector3.up * (thrust * Time.deltaTime), ForceMode.Acceleration);
+
+			if (fuel > 0) {
+				fuel -= 1;
+
+			} else {
+				inFlight = false;
+			}
+
+		} else {
+			if (transform.position.y > 50) {
+				rb.useGravity = true;
+				rb.mass = 0.01f;
+				
+			} else if (transform.position.y < 25) {
+				rb.useGravity = false;
+				fuel = startingFuel;
+			}
+
+		}
+	}
+
+	public void Launch() {
+		inFlight = true;
+	}
+}
