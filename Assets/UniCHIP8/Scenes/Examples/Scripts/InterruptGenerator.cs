da223b3a8a3099d889e0019b7d8b906f6be544bc
Scott Elcomb <psema4@gmail.com>
2018-06-07 02:56:44 -0400

    reorganize demos

---
+++ Assets/UniCHIP8/Scenes/Examples/Scripts/InterruptGenerator.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class InterruptGenerator : UniCHIP8Node {
+	public UniCHIP8 target;
+	public int interruptNumber = 0;
+
+	[Space(10)]
+	public GameObject int0_gameObject1;
+	public GameObject int0_gameObject2;
+	
+	[Space(10)]
+	public string int1_data = "Hello UniCHIP8"; // Fixme: update this value from an InputField
+
+	public void IssueInterrupt() {
+		if (target != null) {
+			switch(interruptNumber) {
+			case 0:
+				// Simulate a collision
+				target.WriteASCIIString(0x20, int0_gameObject1.name);
+				target.WriteASCIIString(0x40, int0_gameObject2.name);
+				break;
+
+			case 1:
+				target.WriteASCIIString(target.dataPortAddress, int1_data);
+				break;
+
+			default:
+				break;
+			}
+
+			target.Interrupt(interruptNumber);
+		}
+	}
+
+	// A receiving UnitCHIP will issue an Interrupt 1 when it the Receive() method is called via a router
+
+	public void SendData() {
+		if (router != null && target != null) {
+			router.SendMessage ("Data", target.name + "|" + int1_data);
+		}
+	}
+
+	public void SendData(string text) {
+		if (router != null && target != null) {
+			router.SendMessage ("Data", target.name + "|" + text);
+		}
+	}
+	
+}
---
+++ Assets/UniCHIP8/Scenes/Examples/Scripts/LightSwitch.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightSwitch : MonoBehaviour {
+
+	public bool switchState = false;
+	public Light targetLight;
+
+	void ToggleSwitch() {
+		targetLight.enabled = !targetLight.enabled;
+	}
+}
